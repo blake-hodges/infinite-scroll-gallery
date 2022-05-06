a6bff0ed208c5e3132210c3b49137cf496455f31
blake-hodges <tbhodges@gmail.com>
2022-05-06 12:25:35 +0800

    add request to unsplash image api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React, { useEffect } from 'react';
+const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 function App() {
-  return (
-    <div>
-        <h1>Infinite Image Gallery</h1>
-    </div>
+    useEffect(() => {
+           fetch(`https://api.unsplash.com/search/photos?query=dogs&client_id=${ACCESS_KEY}`)
+           .then(res => res.json())
+           .then(data => {
+               console.log(data)
+           });
+    })
+    return (
+        <div>
+            <h1>Infinite Image Gallery</h1>
+        </div>
   );
 }
 
